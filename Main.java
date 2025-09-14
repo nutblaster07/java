350fb414ad2f708756666e293fe9599c2a2f1bd4
aroy7187 <aroy7187@gmail.com>
2025-09-14 21:40:52 +0530

    first

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Test{
+    private int a=10;
+    String str="Hello";
+    public void display(){
+        System.out.println("This is display method for private variable a: "+a);
+    }
+}
+public class Main extends Test{
+   public static void main(String[] args){
+      System.out.println("Hello World");
+      Test obj=new Test();
+      obj.display();
+      System.out.println(obj.str);
+   }
+    
+}
